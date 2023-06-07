88bbf9c37d95ef9e430532b516c90208a6511f84
joerhew <joerhew@gmail.com>
2023-06-07 12:30:57 -0400

    Turned every method to work with a linked list instead of an array

--- linkedList.js
+++ linkedList.js
@@ -3,13 +3,14 @@ const LinkedList = () => {
   
   //append(value) adds a new node containing value to the end of the list
   const append = (node) => {
-    if (headNode === null) { //If the list is empty
-      prepend(node);
-    } else if (headNode.nextNode === null) { //If there is only one node in the list
-      headNode.nextNode = node;
-    } else { //If there are multiple nodes in the list
-      let prevEndingNode = traverse().endingNode; //Start from the headNode and traverse down
-      prevEndingNode.nextNode = node;
+    if (headNode === null) {
+      headNode = node
+    } else {
+      let temp = headNode;
+      while (temp.nextNode) {
+        temp = temp.nextNode;
+      }
+      temp.nextNode = node;
     }
   }
   //prepend(value) adds a new node containing value to the start of the list
@@ -22,7 +23,16 @@ const LinkedList = () => {
   }
   //size returns the total number of nodes in the list
   const size = () => {
-    return traverse().array.length;
+    let counter = 0;
+    if (headNode !== null) {
+      counter++;
+      let temp = headNode;
+      while (temp.nextNode) {
+        counter++;
+        temp = temp.nextNode;
+      }
+    }
+    return counter;
   }
   //head returns the first node in the list
   const head = () => {
@@ -30,64 +40,63 @@ const LinkedList = () => {
   }
   //tail returns the last node in the list
   const tail = () => {
-    return traverse().endingNode;
+    if (headNode !== null) {
+      let temp = headNode;
+      while (temp.nextNode) {
+        temp = temp.nextNode
+      }
+      return temp;
+    }
+    return null;
   }
   //at(index) returns the node at the given index
   const at = (index) => {
-    return traverse().array[index];
+    if (index === 0) {
+      return headNode;
+    } else {
+      let temp = headNode;
+      let counter = 0;
+      while (temp.nextNode && counter < index) {
+        counter++;
+        temp = temp.nextNode;
+      }
+      return temp;
+    }
   }
   //pop removes the last element from the list
   const pop = () => {
-    let newTailNodePosition = list.size() - 2; //-2 to account for size starting at 1 and index starting at 0
-    let newTailNode = list.at(newTailNodePosition);
-
+    let newTailNodePosition = size() - 2;
+    let newTailNode = at(newTailNodePosition);
     newTailNode.nextNode = null;
   }
   //contains(value) returns true if the passed in value is in the list and otherwise returns false.
   const contains = (value) => {
-    return !!find(value);
+    return find(value) !== null;
   }
   //find(value) returns the index of the node containing value, or null if not found.
   const find = (value) => {
-    let arrayOfNodeValues = traverse().array.map(node => node.value);
-    let result = arrayOfNodeValues.find(elem => elem === value);
-    if (result) {
-      return result;
-    } else {
-      return null;
-    };
+    let temp = headNode;
+    let counter = 0;
+    while (temp) {
+      if (temp.value === value) {
+        return counter;
+      } else {
+        counter++;
+        temp = temp.nextNode;
+      }
+    }
+    return null;
   }
   //toString represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
   const toString = () => {
     let string = '';
-    let arrayOfNodes = traverse().array;
-    
-    arrayOfNodes.forEach(node => {
-      string += `( ${node.value} )`;
-      
-      if (node.nextNode !== null) {
-        string += ` -> `;
-      }
-    })
-
-    return string;
-  }
-
-  const traverse = () => {
-    let array = [];
-    let endingNode = traverseRecursive(headNode).node;
-
-    function traverseRecursive(node) {
-      if (node.nextNode === null) {
-        array.push(node);
-        return { node };
-      } else {
-        array.push(node)
-        return traverseRecursive(node.nextNode);
-      }
+    let temp = headNode;
+    while (temp) {
+      string += `( ${temp.value} )`
+      if (temp.nextNode) string += ` -> `
+      temp = temp.nextNode;
     }
-
-    return { endingNode, array };
+    return string;
   }
 
   return { append, prepend, size, head, tail, at, pop, contains, find, toString }
@@ -101,25 +110,32 @@ let list = LinkedList();
 const firstNode = Node('Alphabet');
 const secondNode = Node('Bear');
 const thirdNode = Node('Cat');
+const fourthNode = Node('Duck');
+const fifthNode = Node('000');
 
 list.append(firstNode);
 list.append(secondNode);
 list.append(thirdNode);
+list.prepend(fourthNode);
 
 list.pop();
+
+list.append(fifthNode);
 
 console.log('tail: ' + list.tail().value);
 console.log('head: ' + list.head().value);
 
-console.log('first node: ' + firstNode.value);
-console.log('second node: ' + secondNode.value);
-console.log('third node: ' + thirdNode.value);
 console.log('size: ' + list.size());
 
 console.log('at 1 ' + list.at(0).value);
+console.log('at 2 ' + list.at(1).value);
+console.log('at 3 ' + list.at(2).value);
 
 console.log(list.toString());
+console.log(list.find('Alphabet'));
 console.log(list.find('Bear'));
+console.log(list.find('Cat'));
 console.log(list.find('Car'));
 console.log(list.contains('Alphabet'));
+console.log(list.contains('Bear'));
 console.log(list.contains('Car'));
